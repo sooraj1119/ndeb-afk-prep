755df2625802e6d7211d1c50c823829ed3d536c9
Sooraj Bhaskar <rocksuraj.b2@gmail.com>
2026-09-15 20:37:36 -0400

    fix: apply global DOM patch to allow google translate without crashing react

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,22 @@
+// Patch to prevent Google Translate from crashing React
+if (typeof Node === 'function' && Node.prototype) {
+  const originalRemoveChild = Node.prototype.removeChild;
+  Node.prototype.removeChild = function (child) {
+    if (child.parentNode !== this) {
+      if (console) console.warn('Prevented React Google Translate crash (removeChild)');
+      return child;
+    }
+    return originalRemoveChild.apply(this, arguments as any);
+  };
+  const originalInsertBefore = Node.prototype.insertBefore;
+  Node.prototype.insertBefore = function (newNode, referenceNode) {
+    if (referenceNode && referenceNode.parentNode !== this) {
+      if (console) console.warn('Prevented React Google Translate crash (insertBefore)');
+      return newNode;
+    }
+    return originalInsertBefore.apply(this, arguments as any);
+  };
+}
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
@@ -11,3 +30,4 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     </ErrorBoundary>
   </React.StrictMode>,
 )
+
